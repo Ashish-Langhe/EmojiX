bc98d9032d54f0149336999158ead31cd5afe896
Ashish Langhe <ashish.langhe@globant.com>
2023-11-02 19:56:10 +0530

    Initial Commit

---
+++ EmojiX/EmojiXApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiXApp.swift
+//  EmojiX
+//
+//  Created by Ashish Langhe on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
