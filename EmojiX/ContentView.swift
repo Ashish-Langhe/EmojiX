fe5c6a82342fa97ea218bfb7b2ef9c91bc784cc9
Ashish Langhe <ashish.langhe@globant.com>
2023-11-02 21:15:23 +0530

    Updated UI with Halloween emojis

--- EmojiX/ContentView.swift
+++ EmojiX/ContentView.swift
@@ -7,13 +7,34 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case 😺, 🎃, 👻, 💀, 🤖, 👿, 💩, 👽, 🧛, 🧙🏻, 🥷🏻, 🧟‍, 🦹🏼, 🧌, 👩🏻‍🎤
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .🎃
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("Emoji-X")
+                    .font(.system(size: 55))
+                Spacer()
+                Text(selection.rawValue)
+                    .font(.system(size: 250))
+                Text("Halloween Character")
+                    .font(.system(size: 30))
+                Spacer()
+                Text("Select Emoji")
+                    .font(.system(size: 20))
+                    Picker("Select Emoji", selection: $selection) {
+                        ForEach(Emoji.allCases, id: \.self) { emoji in
+                            Text(emoji.rawValue)
+                           }
+                    }
+                    .pickerStyle(.wheel)
+                    .font(.system(size: 80))
+            }
         }
         .padding()
     }
